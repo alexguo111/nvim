52281a7f0cf0db0d3443f409b568e232a16ffce8
Alex Guo <alex@Alexs-MacBook-Pro.local>
2023-03-18 21:59:32 +0800

    change DAP to lldb, because I am using Mac

--- after/plugin/dap_lldp.lua
+++ after/plugin/dap_lldp.lua
@@ -18,6 +18,10 @@ dap.configurations.cpp = {
   },
 }
 
+-- for C and Rust
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
+
 vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
 vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
 vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
@@ -26,7 +30,7 @@ vim.keymap.set('n', '<F9>', function() require('dap').terminate() end)
 vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
 vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
 vim.keymap.set('n', '<Leader>lp',
-function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+  function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
 vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
 vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
 
@@ -41,6 +45,6 @@ vim.keymap.set('n', '<Leader>ds', function()
   widgets.sidebar(widgets.scopes).open()
 end)
 vim.keymap.set('n', '<Leader>df', function()
-      local widgets = require('dap.ui.widgets')
-      widgets.centered_float(widgets.frames)
-    end)
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.frames)
+end)
